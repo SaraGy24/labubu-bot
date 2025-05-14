6906d89ee19c290d67822d595bb316eddaa782e2
SaraGy24 <sari24.gyorfi@gmail.com>
2025-05-14 23:57:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -83,22 +83,6 @@ def check_labubu_stock(url):
         print(f"{url} - NEM található a gomb!")
         return False
 
-# Keep-alive trükk
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot él!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-keep_alive()
-bot.run(TOKEN)
 # Parancs: !ping -> Pong!
 @bot.command()
 async def ping(ctx):
@@ -122,4 +106,20 @@ async def helpme(ctx):
     `!helpme` → Ez a parancs.
     """
     await ctx.send(help_text)
+# Keep-alive trükk
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot él!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+    
+keep_alive()
+bot.run(TOKEN)
 
