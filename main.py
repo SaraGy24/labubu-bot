37539fa51facd211b1e8c7d768736e18e72eeda1
SaraGy24 <sari24.gyorfi@gmail.com>
2025-05-15 15:24:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-chrome_options.binary_location = "/usr/bin/chromium"
+chrome_options.binary_location = "/usr/bin/chromium-browser"
 
 # ÚJ: Service objektum létrehozása és átadása helyesen
 service = Service(ChromeDriverManager().install())
