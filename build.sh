a27af4395663d45df9b49f826ac05eeca56c4c28
SaraGy24 <sari24.gyorfi@gmail.com>
2025-05-15 14:49:28 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Telepítjük a Google Chrome-ot headless módban való futtatáshoz
+
+echo "Installing Google Chrome..."
+apt-get update
+apt-get install -y wget gnupg2
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
+echo "Google Chrome installed!"
