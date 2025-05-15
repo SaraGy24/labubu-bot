390271cd4ef61dfe6f62d9c758c73d16ab829507
SaraGy24 <sari24.gyorfi@gmail.com>
2025-05-15 15:29:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-chrome_options.binary_location = "/usr/bin/chromium-browser"
+chrome_options.binary_location = "/usr/bin/chromium"
 
-# ÚJ: Service objektum létrehozása és átadása helyesen
-service = Service(ChromeDriverManager().install())
+service = Service("/usr/bin/chromedriver")
+
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 import os
