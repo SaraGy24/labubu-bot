0baa19c8d4b35ace3d6efb5a1681f6ff6be0a92b
SaraGy24 <sari24.gyorfi@gmail.com>
2025-05-15 15:36:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")  # ha nem kell ablak
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-chrome_options.binary_location = "/usr/bin/chromium"
-
-service = Service("/usr/bin/chromedriver")
+service = Service(ChromeDriverManager().install())
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
+
+# most már használhatod a drivert...
+
 
 import os
 import discord
