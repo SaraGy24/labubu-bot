7c290b78c964fcfe43ba1e186a33cdf45235157d
SaraGy24 <sari24.gyorfi@gmail.com>
2025-05-15 11:39:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands, tasks
 from flask import Flask
+from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
